92adb3c8d5abbcc8c3f102dfa7d96829cd68f3c5
arhipy97 <kirarh1997@gmail.comm>
2020-10-14 17:49:22 +0300

    init: start online-newspaper task

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    context: path.resolve(__dirname, "src"),
+    mode: "development",
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist')
+    },
+    resolve: {
+        alias: {
+            '@styles': path.resolve(__dirname, "src/styles")
+        }
+    },
+    devServer: {
+        port: 4200
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./index.html"
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader","css-loader"]
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ["file-loader"]
+            },
+            {
+                test:/\.(ttf|woff|woff2|eot)/,
+                use: ["file-loader"]
+            }
+        ]
+    }
+};
